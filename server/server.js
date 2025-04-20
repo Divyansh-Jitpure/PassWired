4a45bebee9508063a13635025561bd7af95b57c5
Divyansh Jitpure <divyanshjit2022@gmail.com>
2025-04-20 22:50:41 +0530

    Update

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 import express from "express";
-
+const PORT = 5000;
 const app = express();
 
-app.listen(5000, () => {
-  console.log("Server is running on port 5000");
+app.listen(PORT, () => {
+  console.log("Server is running on port ", PORT);
 });
