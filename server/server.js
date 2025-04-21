f2b06fd1360ee65494b074eec525c4e224e3de16
Divyansh Jitpure <divyanshjit2022@gmail.com>
2025-04-21 22:53:15 +0530

    update

--- server/server.js
+++ server/server.js
@@ -1,6 +1,15 @@
 import express from "express";
+import cors from "cors";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
 const PORT = 5000;
 const app = express();
+app.use(cors({ origin: "http://localhost:5173" }));
+app.use(express.json());
+// app.use(express.urlencoded({ extended: true }));
 
 app.listen(PORT, () => {
   console.log("Server is running on port ", PORT);
