e44c6d6db655f177dbb91f8b397995886a52c37c
Divyansh Jitpure <divyanshjit2022@gmail.com>
2025-07-11 00:00:57 +0530

    Nav progress

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   HiHome,
   HiOutlineHome,
@@ -7,22 +7,36 @@ import {
   HiLockClosed,
   HiOutlineLockClosed,
 } from "react-icons/hi2";
-import { CgMenuGridO } from "react-icons/cg";
+import { useLocation } from "react-router";
 
 const Navbar = () => {
+  const path = useLocation().pathname;
+
   return (
-    <nav className="fixed bottom-0 grid h-20 w-full grid-cols-3 items-center justify-items-center bg-[#F05454] *:flex *:h-full *:w-full *:items-center *:justify-center *:hover:bg-amber-600">
-      <section className="">
-        <HiOutlineLockClosed className="text-5xl" />
-        {/* <HiLockClosed className="text-5xl" /> */}
+    <nav className="fixed bottom-0 grid h-20 w-full grid-cols-3 items-center justify-items-center gap-5 bg-[#F05454] *:flex *:h-full *:w-full *:flex-col *:items-center *:justify-center *:hover:bg-[#ef3c3c]">
+      <section>
+        {path === "/vault" ? (
+          <HiLockClosed className="text-4xl" />
+        ) : (
+          <HiOutlineLockClosed className="text-4xl" />
+        )}
+        <span className="font-semibold">Vault</span>
       </section>
-      <section className="">
-        <HiOutlineHome className="text-5xl" />
-        {/* <HiHome className="text-5xl" /> */}
+      <section>
+        {path === "/" ? (
+          <HiHome className="text-4xl" />
+        ) : (
+          <HiOutlineHome className="text-4xl" />
+        )}
+        <span className="font-semibold">Home</span>
       </section>
-      <section className="">
-        <HiOutlineSquares2X2 className="text-5xl" />
-        {/* <HiSquares2X2 className="text-5xl" /> */}
+      <section className="flex flex-col">
+        {path === "/more" ? (
+          <HiSquares2X2 className="text-4xl" />
+        ) : (
+          <HiOutlineSquares2X2 className="text-4xl" />
+        )}
+        <span className="font-semibold">More</span>
       </section>
     </nav>
   );
