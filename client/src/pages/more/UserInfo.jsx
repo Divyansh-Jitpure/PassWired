73c3f6894fd40126aae4f6550d8c1da57e81203a
Divyansh Jitpure <divyanshjit2022@gmail.com>
2025-07-23 01:08:34 +0530

    UserInfo update

--- client/src/pages/more/UserInfo.jsx
+++ client/src/pages/more/UserInfo.jsx
@@ -1,27 +1,27 @@
 import React from "react";
 import pic from "/pic2.png";
 import { useNavigate } from "react-router";
+import { useSelector } from "react-redux";
 
 const UserInfo = () => {
+  const user = useSelector((state) => state.auth.user);
   const navigate = useNavigate();
-  const username = "DivyanshJit";
-  const email = "divyanshjit@gmail.com";
   return (
     <div className="mt-5 flex items-center justify-center gap-3">
-      <img
+      {/* <img
         onClick={() => navigate("/profile")}
         className="aspect-square w-16 cursor-pointer rounded-full object-cover"
         src={pic}
         alt="profilePicture"
-      />
+      /> */}
       <section className="flex flex-col">
         <span
           onClick={() => navigate("/profile")}
-          className="w-fit cursor-pointer text-xl font-semibold text-[#30475E]"
+          className="w-fit cursor-pointer text-2xl font-semibold text-[#30475E] text-shadow-md"
         >
-          {username}
+          {user.username}
         </span>
-        <span className="text-[#30475eb7]">{email}</span>
+        <span className="text-[#30475eb7]">{user.email}</span>
       </section>
     </div>
   );
